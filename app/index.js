bd45c32e9a7616ee9f3a28c46a9bec5b0a8049f3
Captain Cool <69457423+Capta1nCool@users.noreply.github.com>
2023-08-11 12:40:22 +0530

    Add files via upload

---
+++ app/index.js
@@ -0,0 +1,279 @@
+require('v8-compile-cache')
+const { app, BrowserWindow, clipboard, protocol, screen, ipcMain, ipcRenderer, shell } = require('electron')
+const path = require('path')
+const fs = require('fs')
+const https = require('https');
+const Store = require('electron-store');
+const userPrefs = new Store({
+    defaults: {
+        fullscreenMode: true,
+        enableUserscripts: true,
+        disableFrameRateLimit: false,
+        disableAdvertisements: true,
+        experimentalFlags: false,
+        lowLatency: false,
+    }
+})
+
+const shortcut = require('electron-localshortcut')
+
+const { autoUpdater } = require("electron-updater")
+let updateLoaded = false;
+let updateNow = false;
+
+const launchArgs = require('./modules/launchArgs');
+launchArgs.pushArguments()
+
+app.allowRendererProcessReuse = true;
+
+protocol.registerSchemesAsPrivileged([{
+    scheme: "swap",
+    privileges: {
+        secure: true,
+        corsEnabled: true
+    }
+}]);
+
+console.log('Chrome:', process.versions.chrome, 'Electron:', process.versions.electron)
+
+class Client {
+    constructor() { }
+
+    init() {
+        this.createWindow()
+        this.initUpdater()
+        this.initShortcuts()
+        this.ipcEvents()
+    }
+
+    createWindow() {
+        const { width, height } = screen.getPrimaryDisplay().workAreaSize
+        this.win = new BrowserWindow({
+            width,
+            height,
+            fullscreen: true,
+            backgroundColor: '#202020',
+            title: 'Tribals Client',
+            icon: __dirname + "/../icon.png",
+            webPreferences: {
+                preload: __dirname + '/preload.js',
+                nodeIntegration: false,
+            }
+        })
+
+        this.win.removeMenu()
+        this.win.setFullScreen(userPrefs.get('fullscreenMode'))
+        this.win.loadURL('https://tribals.io')
+            .catch((error) => console.log(error))
+
+        this.win.on('ready-to-show', () => {
+            this.win.show()
+        })
+
+        this.win.webContents.on('will-prevent-unload', (event) => event.preventDefault())
+
+        this.win.webContents.on('unresponsive', () => {
+            console.log('Client is unresponsive...')
+            this.win.webContents.forcefullyCrashRenderer()
+            this.win.webContents.reload()
+        })
+
+        this.win.webContents.on('render-process-gone', () => {
+            console.log('Client\'s renderer is gone...')
+            this.win.webContents.forcefullyCrashRenderer()
+            this.win.webContents.reload()
+        })
+
+        this.win.webContents.on('new-window', (event, url) => {
+            event.preventDefault()
+            var redirectWindow = new BrowserWindow({
+                width: 800,
+                height: 600,
+                frame: false,
+                minimizable: false,
+                movable: false,
+                transparent: true,
+                webPreferences: {
+                    nodeIntegration: false
+                }
+            });
+
+            redirectWindow.loadURL(url);
+
+
+            this.win.on('focus', () => {
+                redirectWindow.close()
+            })
+        })
+
+        this.win.webContents.on('dom-ready', () => {
+            this.injectScripts()
+            this.replaceResources()
+            this.injectCSS()
+        })
+    }
+
+    initShortcuts() {
+        shortcut.register(this.win, 'F4', () => {
+            this.win.loadURL('https://tribals.io')
+                .catch((error) => console.log(error))
+        })
+        shortcut.register(this.win, 'Escape', () => {
+            this.win.webContents.executeJavaScript('setTimeout(() => {document.exitPointerLock()}, "200");', true)
+        })
+        shortcut.register(this.win, 'F11', () => {
+            this.win.fullScreen = !this.win.fullScreen; userPrefs.set('fullscreenMode', this.win.fullScreen)
+        });
+        shortcut.register(this.win, 'F12', () => {
+            this.win.toggleDevTools()
+        })
+        shortcut.register(this.win, 'F6', () => {
+            if (clipboard.readText().includes("tribals.io")) { this.win.loadURL(clipboard.readText()) }
+        })
+        shortcut.register(this.win, 'F5', () => {
+            this.win.reload()
+        })
+
+    }
+
+    initUpdater() {
+        autoUpdater.setFeedURL({
+            owner: "OnRushStudio",
+            repo: "Tribals-Client",
+            provider: "github",
+            updaterCacheDirName: "tribals-client-updater",
+        });
+
+
+        autoUpdater.checkForUpdates();
+
+        autoUpdater.on('update-available', () => {
+            const options = {
+                title: "Client Update",
+                buttons: ["Now", "Later"],
+                message: "Client Update available, do you want to install it now or after the next restart?",
+                icon: __dirname + "/../icon.ico"
+            }
+            dialog.showMessageBox(options).then((result) => {
+                if (result.response === 0) {
+                    updateNow = true;
+                    if (updateLoaded) {
+                        autoUpdater.quitAndInstall();
+                    }
+                }
+            });
+        });
+
+        autoUpdater.on('update-downloaded', () => {
+            updateLoaded = true;
+            if (updateNow) {
+                autoUpdater.quitAndInstall(true, true);
+            }
+        });
+    }
+
+    injectScripts() {
+        const scriptDirectory = path.normalize(`${app.getPath('documents')}/Tribals Client/Userscript`)
+
+        if (!fs.existsSync(scriptDirectory)) {
+            fs.mkdirSync(scriptDirectory, {
+                recursive: true
+            })
+        }
+
+        const files = fs.readdirSync(scriptDirectory, { withFileTypes: true })
+        if (files.length === 0) return
+
+        files.forEach(file => {
+            if (!file.name.includes('js')) return;
+            let script = fs.readFileSync(scriptDirectory + '/' + file.name, { encoding: 'utf-8' });
+            try {
+                this.win.webContents.executeJavaScript(script);
+            } catch (error) {
+                console.error("an error occurred while executing userscript: " + file + " error: " + error);
+            }
+        });
+    }
+
+    replaceResources() {
+        const AdBlockList = [
+            'pubads.g.doubleclick.net',
+            'video-ad-stats.googlesyndication.com',
+            'simage2.pubmatic.com/AdServer',
+            'pagead2.googlesyndication.com',
+            'securepubads.g.doubleclick.net',
+            'googleads.g.doubleclick.net',
+            'adclick.g.doubleclick.net'
+        ]
+
+        const swapDirectory = path.normalize(`${app.getPath('documents')}/Tribals Client/Resource Swapper`)
+
+        if (!fs.existsSync(swapDirectory)) {
+            fs.mkdirSync(swapDirectory, {
+                recursive: true
+            })
+        }
+
+        this.win.webContents.session.webRequest.onBeforeRequest((details, callback) => {
+            if (AdBlockList.includes(new URL(details.url).hostname) && userPrefs.get('disableAdvertisements')) {
+                return callback({ cancel: true })
+            }
+            if (new URL(details.url).hostname === 'tribals.io' && new URL(details.url).pathname.length > 1) {
+                const url = path.join(swapDirectory, new URL(details.url).pathname);
+                if (fs.existsSync(url)) {
+                    return callback({ cancel: false, redirectURL: `swap:/${url}` })
+                }
+            }
+            return callback({ cancel: false })
+        })
+    }
+
+    injectCSS() {
+        let cssDirectory = path.normalize(`${app.getPath('documents')}/Tribals Client/CSS`)
+
+        if (!fs.existsSync(cssDirectory)) {
+            fs.mkdirSync(cssDirectory, {
+                recursive: true
+            });
+        }
+
+        const files = fs.readdirSync(cssDirectory, { withFileTypes: true })
+        if (files.length === 0) return
+
+        files.forEach(file => {
+            if (!file.name.includes('css')) return;
+            let css = fs.readFileSync(cssDirectory + '/' + file.name, { encoding: 'utf-8' });
+            try {
+                this.win.webContents.on('did-finish-load', () => {
+                    this.win.webContents.insertCSS(css)
+                })
+            } catch (error) {
+                console.log(error)
+            }
+        });
+    }
+
+    ipcEvents() {
+        ipcMain.on('exit', () => {
+            app.quit()
+        })
+    }
+}
+
+const client = new Client()
+
+app.on('ready', () => {
+    protocol.registerFileProtocol('swap', (request, callback) => {
+        callback({
+            path: path.normalize(request.url.replace(/^swap:/, ''))
+        })
+    })
+
+    if (app.requestSingleInstanceLock()) client.init()
+})
+
+app.on('window-all-closed', () => app.exit())
+
+process.on('uncaughtException', (error) => {
+    console.log(error)
+})
---
+++ app/preload.js
@@ -0,0 +1,165 @@
+const { ipcRenderer, clipboard } = require('electron');
+const Store = require('electron-store')
+const userPrefs = new Store()
+
+var clientDiv
+const settingsJson = require('./modules/settings.json')
+
+document.addEventListener('DOMContentLoaded', () => {
+    clientDiv = document.createElement('div');
+    clientDiv.innerHTML = `
+    <style>
+        .sidebar-wrapper::before {
+            content: "";
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background: rgba(0, 0, 0, 0.3);
+            backdrop-filter: blur(5px);
+            z-index: 4;
+        }
+        #sidebar {
+            position: fixed;
+            width: 22rem;
+            height: 100vh;
+            right: 0;
+            top: 0;
+            background: #101010;
+            z-index: 5;
+            color: white;
+            text-align: center;
+        }
+        label {
+            cursor: pointer;
+            position: absolute;
+            left: 1rem;
+        }
+        .field-cont {
+            font-size: 1.2rem;
+            width: 22rem;
+            align-items: center;
+            padding-top: 1.5rem;
+            height: 3rem;
+        }
+        .field-cont:hover {
+            background: rgb(75, 138, 23);
+        }
+        .client-toggle {
+            right: 1rem;
+            position: absolute;
+            cursor: pointer;
+        }
+    </style>
+    <div id="sidebar">
+    <div id="blur-cont"></div>
+        <div style="padding-bottom: 1.5rem; padding-top: 1.5rem; font-size: 1.5rem;"> Client Settings </div>
+        <div id="clientContent"></div>
+    </div>
+    `;
+    clientDiv.style.display = "none";
+    clientDiv.classList.add('sidebar-wrapper')
+    document.body.appendChild(clientDiv)
+
+    Object.keys(settingsJson).forEach((setting) => {
+        document.getElementById('clientContent').innerHTML += `
+        <div class="field-cont">
+            <label for="${setting}">${settingsJson[setting].text}</label>
+            <input type="checkbox" id="${setting}" ${userPrefs.get(setting) ? 'checked' : ''} class="client-toggle" onclick=window.setSetting("${setting}")>
+        </div>
+        `
+    })
+
+    const observer = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+            mutation.addedNodes.forEach((node) => {
+                if (node.id == "menu" || node.id == "play-section") {
+                    if (!document.getElementById("closeClientBtn")) {
+                        let menuCont = document.querySelector("#social-links");
+                        let closeClient = document.createElement("a")
+                        closeClient.id = "closeClientBtn";
+                        closeClient.innerHTML = `<i aria-hidden="true" class="fa fa-times"></i>`
+                        menuCont.appendChild(closeClient)
+                        closeClient.onclick = () => {
+                            ipcRenderer.send("exit")
+                        }
+                    }
+                    if (!document.getElementById("JoinBtn")) {
+                        let joinLink = document.createElement("div")
+                        joinLink.innerHTML = `
+                        <button class="invite-button" style="background:linear-gradient(to bottom, rgb(255,165,0), rgb(190,123,0)); border-left: solid 8px rgb(170,110,0)" id="JoinBtn">
+                            <i aria-hidden="true" class="fa fa-link">
+                        </i> Join Link</button>
+                        `
+                        let contDiv = document.querySelector("#play-section")
+                        contDiv.insertBefore(joinLink, contDiv.children[1])
+                        joinLink.onclick = () => {
+                            if (!document.getElementById('linkDiv')) {
+                                var linkDiv = document.createElement('div')
+                                // linkDiv.id = "linkDiv";
+                                linkDiv.innerHTML = `<div id="linkDiv"
+                                style="align-items: center;width: 500px;height: 150px;background: #101010;color: white;position: absolute;top: 50%;left: 50%;padding: 10px;border-radius: 10px;transform: translate(-50%, -50%);">
+                                <h2 style="text-align: center; align-self: center;">ENTER LINK TO JOIN</h2>
+                                <input
+                                    style="position: absolute;left: 6rem;background: transparent;border: 1px solid white;height: 1.5rem;width: 20rem;color: white;border-radius: 3px;"
+                                    id="linkInput" type="text">
+                                <button id="linkBtn" style="position: absolute;top: 80%;left: 50%;transform: translate(-50%, -50%);border: none;background: rgb(255,165,0);color: white;width: 10rem;padding: 0.5rem;cursor: pointer;">JOIN</button>
+                                <i id="linkClose" class="fa fa-times" aria-hidden="true"
+                                    style="position: fixed;top: 0.5rem;cursor: pointer;right: 0.75rem;"></i>
+                            </div>`
+                                document.body.appendChild(linkDiv)
+                                document.getElementById('linkBtn').onclick = () => {
+                                    if (document.getElementById('linkInput').value.includes("tribals.io")) {
+                                        window.location.href = clipboard.readText()
+                                    }
+                                }
+                                document.getElementById('linkClose').onclick = () => {
+                                    document.body.removeChild(linkDiv)
+                                }
+                            }
+                            // if (clipboard.readText().includes("tribals.io")) {
+                            //     window.location.href = clipboard.readText()
+                            // } else {
+                            //     alert('No valid link found on clipboard')
+                            // }
+                        }
+                    }
+                }
+            });
+        });
+    });
+
+    observer.observe(document.documentElement, {
+        childList: true,
+        subtree: true
+    });
+})
+
+window.setSetting = function (name) {
+    console.log(name)
+    var currentValue = userPrefs.get(name);
+    userPrefs.set(name, !currentValue);
+}
+
+document.addEventListener('keydown', (e) => {
+    if (e.code === "F7") {
+        if (clientDiv.style.display === "none") {
+            clientDiv.style.display = "block"
+        } else {
+            clientDiv.style.display = "none"
+        }
+    }
+})
+
+window._pc = false;
+Object.defineProperty(window, "pc", {
+    set(value) {
+        if (!window.pc) {
+            window._pc = value;
+        }
+    },
+    get() {
+        return (window._pc);
+    }
+});
